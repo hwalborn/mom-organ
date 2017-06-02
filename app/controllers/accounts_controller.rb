e5fdf28f1e579486d3f7ec8cdb2d4716c0098970
hwalborn <hwalborn@gmail.com>
2017-06-02 14:43:02 -0400

    can log into Spotify

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,8 @@
 class AccountsController < ApplicationController
   def spotify
     spotify_user = RSpotify::User.new(request.env['omniauth.auth'])
-    byebug
+    session[:spotify_token] = spotify_user.credentials.token
+    flash[:notice] = "Logged into Spotify"
+    redirect_to musics_path
   end
 end
